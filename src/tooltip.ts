5e75eabf916547c3f025b4da6d19afc099fd144c
Michal Srb <xixixao@seznam.cz>
2022-01-20 01:24:33 +0100

    Fix tooltip not showing on first position
    I have editor with 0 padding and absolutely positioned tooltips. The autocomplete tooltip was not showing because it's x position was the same as editor left. This should be strict inequality. I don't have a minimal repro but we could probably put it together easily. I'm not sure about the other inequalities and whether they should be fixed too.

--- src/tooltip.ts
+++ src/tooltip.ts
@@ -234,7 +234,7 @@ const tooltipPlugin = ViewPlugin.fromClass(class {
       // Hide tooltips that are outside of the editor.
       if (!pos || pos.bottom <= Math.max(editor.top, space.top) ||
           pos.top >= Math.min(editor.bottom, space.bottom) ||
-          pos.right <= Math.max(editor.left, space.left) ||
+          pos.right < Math.max(editor.left, space.left) ||
           pos.left >= Math.min(editor.right, space.right)) {
         dom.style.top = Outside
         continue
